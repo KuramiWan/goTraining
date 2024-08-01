370ddea0cb0747fddaef19097431762168048a2f
kurami <kurami.noya@gmail.com>
2024-08-01 23:33:22 +0800

    feat Player旋转

--- player.go
+++ player.go
@@ -13,6 +13,7 @@ type Vector struct {
 type Player struct {
 	playPosition Vector
 	sprite       *ebiten.Image
+	rotation     float64
 }
 
 func newPlayer() Player {
@@ -27,13 +28,22 @@ func newPlayer() Player {
 
 func (p *Player) Update() {
 	p.movement()
+	p.rotate()
 }
 
 func (p *Player) Draw(screen *ebiten.Image) {
 	options := &ebiten.DrawImageOptions{}
+	bounds := p.sprite.Bounds()
+	halfW := float64(bounds.Dx()) / 2
+	halfH := float64(bounds.Dy()) / 2
+	options.GeoM.Translate(-halfW, -halfH)
+	options.GeoM.Rotate(p.rotation)
+	options.GeoM.Translate(halfW, halfH)
 	options.GeoM.Translate(p.playPosition.X, p.playPosition.Y)
 	screen.DrawImage(p.sprite, options)
 }
+
+// update
 func (p *Player) movement() {
 	speed := 5.0
 	var vector Vector
@@ -42,16 +52,26 @@ func (p *Player) movement() {
 		vector.X *= factor
 		vector.Y *= factor
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+	if ebiten.IsKeyPressed(ebiten.KeyUp) || ebiten.IsKeyPressed(ebiten.KeyW) {
 		p.playPosition.Y -= speed
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+	if ebiten.IsKeyPressed(ebiten.KeyDown) || ebiten.IsKeyPressed(ebiten.KeyS) {
 		p.playPosition.Y += speed
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) || ebiten.IsKeyPressed(ebiten.KeyA) {
 		p.playPosition.X -= speed
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+	if ebiten.IsKeyPressed(ebiten.KeyRight) || ebiten.IsKeyPressed(ebiten.KeyD) {
 		p.playPosition.X += speed
+	}
+}
+
+func (p *Player) rotate() {
+	speed := math.Pi / float64(ebiten.TPS())
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButton0) {
+		p.rotation -= speed
+	}
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButton2) {
+		p.rotation += speed
 	}
 }
